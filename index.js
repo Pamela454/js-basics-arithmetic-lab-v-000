80d861098764da770adfb97444ab53eb40ba737b
Pamela Frieze <pfrieze@gmail.com>
2019-01-24 14:27:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ let randomNumber = Math.random();
 
 let randomInteger = Math.floor(randomNumber);
 
-let randomUserID = Math.floor(Math.random() * 1000000020) + 1000000001;
+let randomUserID = Math.floor(Math.random(randomInteger) * 1000000020) + 1000000001;
